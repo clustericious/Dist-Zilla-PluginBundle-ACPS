6871478a707f880df3c7d1698b7618e11f3acdad
Graham Ollis <gollis@sesda2.com>
2012-07-26 16:20:17 -0400

    put the spec template in one place.

---
+++ lib/Dist/Zilla/Plugin/ACPS/RPM.pm
@@ -0,0 +1,58 @@
+package Dist::Zilla::Plugin::ACPS::RPM;
+
+use Moose;
+use v5.10;
+use Dist::Zilla::MintingProfile::ACPS;
+
+# VERSION
+
+extends 'Dist::Zilla::Plugin::RPM';
+
+use namespace::autoclean;
+
+has '+spec_file' => (
+  is => 'ro',
+  default => sub { Dist::Zilla::MintingProfile::ACPS->profile_dir . '/default/dist.spec' },
+);
+
+sub mk_spec {
+    my($self,$archive) = @_;
+    
+    # this is different from the superclass, we allow fully qualified filenames, because
+    # we want to keep the spec template in the share directory with the other profile
+    # stuff.
+    my $spec_file = $self->spec_file =~ /^\// ? $self->spec_file : $self->zilla->root->file($self->spec_file);
+    
+    my $t = Text::Template->new(
+        TYPE       => 'FILE',
+        SOURCE     => $spec_file,
+        DELIMITERS => [ '<%', '%>' ],
+    ) || $self->log_fatal($Text::Template::ERROR);
+    return $t->fill_in(
+        HASH => {
+            zilla   => \($self->zilla),
+            archive => \$archive,
+        },
+    ) || $self->log_fatal($Text::Template::ERROR);
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Dist::Zilla::Plugin::ACPS::RPM - RPM Dist::Zilla plugin for ACPS
+
+=head1 DESCRIPTION
+
+This in herits from L<Dist::Zilla::Plugin::RPM>, and changes the
+default spec template.
+
+=head1 AUTHOR
+
+Graham Ollis <gollis@sesda2.com>
+
+=cut
--- t/use.t
+++ t/use.t
@@ -1,8 +1,9 @@
 use strict;
 use warnings;
-use Test::More tests => 4;
+use Test::More tests => 5;
 
 use_ok 'Dist::Zilla::PluginBundle::ACPS';
 use_ok 'Dist::Zilla::Plugin::ACPS::Release';
 use_ok 'Dist::Zilla::Plugin::ACPS::Init';
+use_ok 'Dist::Zilla::Plugin::ACPS::RPM';
 use_ok 'Dist::Zilla::MintingProfile::ACPS';
